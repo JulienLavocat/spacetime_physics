608e3f067ebbd37a67d46ddeb1a8404c849310e4
Julien Lavocat <julien.lavocat@gmail.com>
2025-06-11 10:26:32 +0200

    feat: add multiple world support

--- spacetime_physics/src/lib.rs
+++ spacetime_physics/src/lib.rs
@@ -1,37 +1,31 @@
-use std::time::Duration;
-
+use log::debug;
 use math::vec3::Vec3;
-use spacetimedb::{reducer, ReducerContext, ScheduleAt, Table, TimeDuration};
-use tables::{physics_rigid_bodies, physics_world, PhysicsWorld, RigidBody};
+use spacetimedb::{reducer, ReducerContext, ScheduleAt};
+use tables::{physics_rigid_bodies, PhysicsWorld, RigidBody};
 
 mod math;
 mod tables;
 
 #[reducer(init)]
 fn init(ctx: &ReducerContext) {
-    ctx.db.physics_world().insert(PhysicsWorld {
-        id: 0,
-        scheduled_at: ScheduleAt::Interval(TimeDuration::from_duration(Duration::from_millis(
-            1000 / 60,
-        ))),
-        gravity: Vec3::new(0.0, -9.81, 0.0),
-    });
+    let world_id = PhysicsWorld::insert(ctx, 60.0, Vec3::new(0.0, -9.81, 0.0)).id;
 
-    ctx.db.physics_rigid_bodies().insert(RigidBody {
-        id: 0,
-        position: Vec3::ZERO,
-        velocity: Vec3::ZERO,
-        force: Vec3::ZERO,
-        mass: 1.0,
-    });
-
-    ctx.db.physics_rigid_bodies().insert(RigidBody {
-        id: 0,
-        position: Vec3::ZERO,
-        velocity: Vec3::ZERO,
-        force: Vec3::ZERO,
-        mass: 2.0,
-    });
+    RigidBody::insert(
+        ctx,
+        world_id,
+        Vec3::new(0.0, 0.0, 0.0),
+        Vec3::ZERO,
+        Vec3::ZERO,
+        1.0,
+    );
+    RigidBody::insert(
+        ctx,
+        world_id,
+        Vec3::new(0.0, 0.0, 0.0),
+        Vec3::ZERO,
+        Vec3::ZERO,
+        2.0,
+    );
 }
 
 #[reducer]
@@ -41,7 +35,7 @@ fn physics_step_world(ctx: &ReducerContext, world: PhysicsWorld) {
         ScheduleAt::Time(time) => ctx.timestamp.duration_since(time).unwrap().as_secs_f32(),
     };
 
-    for mut body in ctx.db.physics_rigid_bodies().iter() {
+    for mut body in ctx.db.physics_rigid_bodies().world_id().filter(world.id) {
         if body.mass > 0.0 {
             body.force += world.gravity * body.mass;
             body.velocity += body.force / body.mass * delta_time;
@@ -50,6 +44,6 @@ fn physics_step_world(ctx: &ReducerContext, world: PhysicsWorld) {
 
         body.force = Vec3::ZERO;
 
-        ctx.db.physics_rigid_bodies().id().update(body);
+        body.update(ctx);
     }
 }
--- spacetime_physics/src/tables.rs
+++ spacetime_physics/src/tables.rs
@@ -1,6 +1,6 @@
 use std::fmt::Display;
 
-use spacetimedb::{table, ScheduleAt};
+use spacetimedb::{table, ReducerContext, ScheduleAt, Table};
 
 use crate::{math::vec3::Vec3, physics_step_world};
 
@@ -13,15 +13,65 @@ pub struct PhysicsWorld {
     pub gravity: Vec3,
 }
 
+impl PhysicsWorld {
+    pub fn insert(ctx: &ReducerContext, tps: f32, gravity: Vec3) -> Self {
+        let scheduled_at = ScheduleAt::Interval(spacetimedb::TimeDuration::from_duration(
+            std::time::Duration::from_secs_f32(1.0 / tps),
+        ));
+
+        ctx.db.physics_world().insert(Self {
+            id: 0,
+            scheduled_at,
+            gravity,
+        })
+    }
+}
+
+impl Display for PhysicsWorld {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "PhysicsWorld(id={}, scheduled_at={:?}, gravity={})",
+            self.id, self.scheduled_at, self.gravity
+        )
+    }
+}
+
 #[table(name = physics_rigid_bodies, public)]
 pub struct RigidBody {
     #[primary_key]
     #[auto_inc]
     pub id: u64,
+    #[index(btree)]
+    pub world_id: u64, // Foreign key to PhysicsWorld
     pub position: Vec3,
     pub velocity: Vec3,
     pub force: Vec3,
     pub mass: f32,
+}
+
+impl RigidBody {
+    pub fn insert(
+        ctx: &ReducerContext,
+        world_id: u64,
+        position: Vec3,
+        velocity: Vec3,
+        force: Vec3,
+        mass: f32,
+    ) -> Self {
+        ctx.db.physics_rigid_bodies().insert(Self {
+            id: 0,
+            world_id,
+            position,
+            velocity,
+            force,
+            mass,
+        })
+    }
+
+    pub fn update(self, ctx: &ReducerContext) -> Self {
+        ctx.db.physics_rigid_bodies().id().update(self)
+    }
 }
 
 impl Display for RigidBody {
